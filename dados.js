7d2347948c9085e2fc1c890aa3240976df00be92
Silvio-Henrique <silviohfabiano@gmail.com>
2024-09-09 00:18:03 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,23 @@
+let atletas = [
+    {
+        nome: "Rayssa Leal",
+        modalidade: "Skate",
+        descricao: "Rayssa Leal, também conhecida como Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento precoce. Aos 13 anos, tornou-se a mais jovem medalhista olímpica da história do Brasil. Com seu estilo único e carisma, Rayssa é um ícone da nova geração do skate e inspira milhões de jovens.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate,fada,sk8"
+    },
+    {
+        nome: "Rebeca Andrade",
+        modalidade: "Ginástica",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o coração dos brasileiros com sua habilidade e determinação. Bicampeã olímpica, ela se tornou um símbolo de superação e inspiração para muitos jovens atletas. Com suas acrobacias impecáveis e carisma contagiante, Rebeca representa o futuro brilhante da ginástica brasileira e já é considerada uma das maiores atletas da história do país.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginasta,ouro"
+    },
+    {
+        nome: "Beatriz Souza",
+        modalidade: "Judô",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destaca no cenário mundial. Com sua técnica apurada e determinação, ela conquistou diversas medalhas em competições internacionais. Beatriz é um exemplo de atleta de alto nível e inspira jovens judocas a seguirem seus sonhos.",
+        link: "https://pt.wikipedia.org/wiki.org/wiki/Beatriz_Souza",
+        tags : "ippon, medalha"
+    }
+];
